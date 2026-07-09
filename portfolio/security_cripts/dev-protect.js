02e0b5cb5a3456f549e7a58a963072b3e7bc118f
Ritik Kumar <146592590+ritik3228@users.noreply.github.com>
2026-07-10 01:48:36 +0530

    Delete portfolio directory

--- portfolio/security_cripts/dev-protect.js
+++
@@ -1,8 +0,0 @@
-// Detect DevTools opening
-let devtools = /./;
-devtools.toString = function() {
-    document.body.innerHTML = '<h1>Developer Tools Detected</h1><p>Inspection is not allowed on this page.</p>';
-    window.location.reload();
-};
-
-console.log('%c', devtools);
